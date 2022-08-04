fc2db1968c0abb5f474a0a58dd191c1ef12d6f7a
Esan Alorizi <esanorizi@outlook.com>
2022-08-05 00:30:06 +0100

    Details Handler added

---
+++ Application/Customers/Details.cs
@@ -0,0 +1,34 @@
+﻿using Domain;
+using MediatR;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Customers
+{
+    public class Details
+    {
+        public class Query : IRequest<Customer>
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Customer>
+        {
+            private readonly DataContext context;
+
+            public Handler(DataContext context)
+            {
+                this.context = context;
+            }
+
+            public async Task<Customer> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await this.context.Customers.FindAsync(request.Id);
+            }
+        }
+    }
+}
